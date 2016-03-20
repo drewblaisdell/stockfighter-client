3d7ca93ba17ced0e82b688300f4eaaa255a78b10
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-03-19 18:06:28 -0700

    React hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
 module.exports = {
-  'test': {}
+  context: __dirname + '/app',
+  entry: {
+    javascript: './index.js',
+    html: './index.html',
+  },
+
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/dist'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '', '.json', '.js', '.jsx' ]
+  }
 };
